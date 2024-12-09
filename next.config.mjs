118df8fa43f8d6c1254a50dd066cbce5ac118feb
Jonathan Leitner <leitnj@gmail.com>
2024-12-09 12:26:06 -0500

    configured port to work with Azure Web Services

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    serverRuntimeConfig: {
+        port: process.env.PORT || 8080,
+    },
+};
 
 export default nextConfig;
